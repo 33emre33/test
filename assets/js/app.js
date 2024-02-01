372a10fd75c61a970c1c47fd8611655efa6a1c27
Emre Eroğlu <emre1eroglu7@gmail.com>
2024-02-01 21:23:15 +0300

    test33

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,6 +23,10 @@ function navClose(){
 function lightMode(){
    
    document.querySelector("body").style.color="black";
+
+   document.querySelector(".dark-button").style.display="block";
+   document.querySelector(".light-button").style.display="none";
+   
    document.querySelector("body").style.background="#e7e7e7";
    document.querySelector(".navigation-wrapper").style.fontWeight="bold";
    document.querySelector(".logo-word-two").style.fontWeight="bold";
@@ -30,15 +34,42 @@ function lightMode(){
    document.querySelector(".nav-item-2").style.borderColor="black";
    document.querySelector(".nav-item-3").style.borderColor="black";
    document.querySelector(".nav-item-4").style.borderColor="black";
+   document.querySelector(".plane-wrapper ").style.color="black";
+
+   document.querySelector(".profile-img").style.borderColor="black";
 
    
 }
 
 function darkMode(){
    
+   document.querySelector(".dark-button").style.display="none";
+   document.querySelector(".light-button").style.display="block";
     document.querySelector("body").style.color="white";
+    
+    document.querySelector(".plane-wrapper ").style.color="white";
     document.querySelector("body").style.background="#000c1a";
     document.querySelector(".navigation-wrapper").style.fontWeight="regular";
     document.querySelector(".logo-word-two").style.fontWeight="regular";
-    
+
+    document.querySelector(".nav-item-1").style.borderColor="white";
+    document.querySelector(".nav-item-2").style.borderColor="white";
+    document.querySelector(".nav-item-3").style.borderColor="white";
+    document.querySelector(".nav-item-4").style.borderColor="white";
+    document.querySelector(".profile-img").style.borderColor="white"; 
  }
+
+ function delay (URL) {
+   setTimeout( function() { window.location = URL }, 5000 );
+}
+
+function planePlayOver(){
+
+   
+   document.querySelector(".plane-wrapper").style.display="block";
+}
+function planeStopOver(){
+
+   
+   document.querySelector(".plane-wrapper").style.display="none";
+}
